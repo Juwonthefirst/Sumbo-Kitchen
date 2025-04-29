393947eb561c5a746ca2b3dcf9325e3106796ed8
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-04-29 22:06:49 +0100

    update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import {renderMenuPage} from './page_renderers/Menu.js'
 import {recommendedFoods, menu} from './foodStore.js';
 
 const contentDiv = document.querySelector('#content')
-const menuPage = renderMenuPage(recommendedFoods, menuf)
+const menuPage = renderMenuPage(recommendedFoods, menu)
 contentDiv.appendChild(menuPage)

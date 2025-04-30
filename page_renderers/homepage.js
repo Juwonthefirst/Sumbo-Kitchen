4d61e4c31d682c4d5f11fbd48fd0f152ed43be2b
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-04-30 10:52:58 +0100

    update

--- page_renderers/homepage.js
+++ page_renderers/homepage.js
@@ -1,3 +1,23 @@
 export const renderHomePage = function() {
-	console.log('juwon')
+	const homeDiv = document.createElement('div')
+	homeDiv.className = 'homepage'
+		const messageDiv = document.createElement('div')
+		messageDiv.className = 'message'
+			const introDiv = document.createElement('div')
+			introDiv.className = 'intro'
+				const greetingTag = document.createElement('p')
+				greetingTag.innerText = 'Welcome to'
+				introDiv.appendChild(greetingTag)
+				const brandTag = document.createElement('h3')
+				brandTag.innerText = 'Sumbo Kitchen' 
+				introDiv.appendChild(brandTag)
+			messageDiv.appendChild(introDiv)
+			const mottoDiv = document.createElement('div')
+			mottoDiv.className = 'motto'
+				const mottoTag = document.createElement('p')
+				mottoTag.innerText = 'Where Food is more than a need, it is a lifestyle'
+				mottoDiv.appendChild(mottoTag)
+			messageDiv.appendChild(mottoDiv)
+		homeDiv.appendChild(messageDiv)
+	return homeDiv
 }

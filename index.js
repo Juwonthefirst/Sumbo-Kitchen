9b8b65bd050423441a74093b4abba4dce3c975e6
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-04-30 15:49:50 +0100

    update

--- index.js
+++ index.js
@@ -7,16 +7,10 @@ const homeBtn = document.querySelector('#home-btn')
 const menuBtn = document.querySelector('#menu-btn')
 const contactBtn = document.querySelector('#about-btn')
 
-homeBtn.addEventListener('click', () => {
-	renderHomePage()
-})
+homeBtn.addEventListener('click', renderHomePage)
 
-menuBtn.addEventListener('click', () => {
-	renderMenuPage()
-})
+menuBtn.addEventListener('click', renderMenuPage)
 
-contactBtn.addEventListener('click', () => {
-	renderContactPage()
-})
+contactBtn.addEventListener('click', renderContactPage)
 
 //homeBtn.click()
--- page_renderers/Menu.js
+++ page_renderers/Menu.js
@@ -61,4 +61,3 @@ export const renderMenuPage = function() {
 	contentDiv.innerText = ''
 	contentDiv.appendChild(menuDiv)
 }
-

6f4eb16180b4a2bbdce70c28251fb45ec4a452da
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-04-30 08:43:45 +0100

    update

--- page_renderers/Menu.js
+++ page_renderers/Menu.js
@@ -1,9 +1,9 @@
 const displayFood = function (foods){
 	const displayDiv = document.createElement('div')
-	displayDiv.classList.add('display')
+	displayDiv.className = 'display'
 	for (let food of foods){
 		const dishDiv = document.createElement('div')
-		dishDiv.classList.add('dish')
+		dishDiv.className = 'dish'
 			const dishImg = document.createElement('img')
 			dishImg.src = food.img
 			dishDiv.appendChild(dishImg)
@@ -25,7 +25,7 @@ const displayFood = function (foods){
 
 const renderRecommendationDiv = function(recommendedFoods) {
 	const recommendationDiv = document.createElement('div');
-	recommendationDiv.classList.add('recommendations');
+	recommendationDiv.className = 'recommendations';
 	const recommendationsHeading = document.createElement('h3');
 	recommendationsHeading.innerText = 'Recommendations for you';
 	recommendationDiv.appendChild(recommendationsHeading);
@@ -47,7 +47,7 @@ const renderMainMenuDiv = function(menu) {
 
 export const renderMenuPage = function(recommendedFoods, menu) {
 	const menuDiv = document.createElement('div');
-	menuDiv.setAttribute('id', 'menupage')
+	menuDiv.id = 'menupage'
 	const recommendationDiv = renderRecommendationDiv(recommendedFoods)
 	const mainMenuDiv = renderMainMenuDiv(menu)
 	menuDiv.appendChild(recommendationDiv)

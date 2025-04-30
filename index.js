29edc31f52d41656beb4ba25634c6f1e854a1f2d
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-04-30 09:32:21 +0100

    update

--- index.js
+++ index.js
@@ -2,5 +2,22 @@ import {renderMenuPage} from './page_renderers/Menu.js'
 import {recommendedFoods, menu} from './foodStore.js'
 
 const contentDiv = document.querySelector('#content')
-const menuPage = renderMenuPage(recommendedFoods, menu)
-contentDiv.appendChild(menuPage)
+const homeBtn = document.querySelector('#home-btn')
+const menuBtns = document.querySelectorAll('.menu-btn')
+const contactBtn = document.querySelector('#about-btn')
+homeBtn.addEventListener('click', () => {
+	contentDiv.innerHTML = ''
+	
+})
+
+menuBtns.forEach((button) => {
+	button.addEventListener('click', () => {
+		contentDiv.innerHTML = ''
+		const menuPage = renderMenuPage(recommendedFoods, menu)
+		contentDiv.appendChild(menuPage)
+	})
+})
+
+contactBtn.addEventListener('click', () => {
+	contentDiv.innerHTML = ''
+})
